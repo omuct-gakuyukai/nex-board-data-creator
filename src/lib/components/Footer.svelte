1fe395123a75d32de453bd14123a0faaafb4f6fd
rirannda <gorirariranndaomuct@gmail.com>
2026-05-25 16:50:40 +0900

    feat: add Header, Footer, Modal, Preview, and Property components with layout integration

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,3 @@
+<div class="mt-auto bg-blue-400 text-center text-white">
+	© 大阪公立大学工業高等専門学校 学友会執行部
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import Preview from '$lib/components/Preview.svelte';
+	import Property from '$lib/components/Property.svelte';
+</script>
+
+<Property />
+<Preview />
